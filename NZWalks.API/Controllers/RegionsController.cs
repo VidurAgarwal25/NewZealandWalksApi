edf54825e7b1346edeb7e5d9278e61e0c3945206
Vidur Agarwal <geurecruitviduragarwal@gmail.com>
2023-10-01 20:49:27 +0530

    without dto

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDBContext dbContext;
+        public RegionsController(NZWalksDBContext dBContext)
+        {
+            this.dbContext = dBContext;
+        }
+        // Get All Regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+
+        //  Get Regions By Id
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute]Guid id)
+        {
+            //var region = dbContext.Regions.Find(id);
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            return Ok(region);
+        }
+
+    }
+}
